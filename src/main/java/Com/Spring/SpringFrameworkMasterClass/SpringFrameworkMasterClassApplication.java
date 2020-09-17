4e424b21577e32fd86aa64fc90c54a6634ee791e
Johirul-Amin-Rizve <johirulaminrizve@gmail.com>
2020-09-17 16:44:06 +0600

    initial commit

---
+++ src/main/java/Com/Spring/SpringFrameworkMasterClass/SpringFrameworkMasterClassApplication.java
@@ -0,0 +1,13 @@
+package Com.Spring.SpringFrameworkMasterClass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkMasterClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkMasterClassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Com/Spring/SpringFrameworkMasterClass/SpringFrameworkMasterClassApplicationTests.java
@@ -0,0 +1,13 @@
+package Com.Spring.SpringFrameworkMasterClass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkMasterClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
